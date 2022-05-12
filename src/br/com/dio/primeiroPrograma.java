6e435c394190991831e9f31968eb5a5648e10d46
edu-gui <e.guilherme100@gmail.com>
2022-05-12 12:12:00 -0300

    criando class livro

--- src/br/com/dio/primeiroPrograma.java
+++ src/br/com/dio/primeiroPrograma.java
@@ -5,11 +5,46 @@ import br.com.dio.model.Gato;
 public class primeiroPrograma {
     public static void main(String[] args){
       Gato  gato = new Gato();
+      System.out.println(gato);
 
-        System.out.println(gato);
+        livro livro = new livro("o iluminado", 300);
+        System.out.println(livro);
 
         /*int a= 5;
         int b = 6;
         System.out.println("hello world" + (a+b));*/
+    }
+    static class livro{
+        private String nome;
+        private Integer numPaginas;
+
+        public livro(String nome, Integer numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(Integer numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+
+        @Override
+        public String toString() {
+            return "livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPaginas=" + numPaginas +
+                    '}';
+        }
     }
 }
